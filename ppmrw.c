002c73d4242cbb404fe6bf35f920b910a6428200
jjg297 <garciajj730@gmail.com>
2016-09-13 13:45:57 -0700

    Add files via upload

---
+++ ppmrw.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 	Jesus Garcia
+	Project 1 - Images - 9/15/16
+	CS430 - Prof. Palmer
+	"Code should be able to read or write P3 or P6 ppm files."	*/
+
+int main(){
+	printf("Hello World");
+	return(0);
+}
