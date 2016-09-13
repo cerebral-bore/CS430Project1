fea2b4a083a0f1bd8bbef4f128f137db6b4928ce
jjg297 <garciajj730@gmail.com>
2016-09-13 13:57:25 -0700

    Add files via upload

--- ppmrw.c
+++ ppmrw.c
@@ -6,7 +6,12 @@
 	CS430 - Prof. Palmer
 	"Code should be able to read or write P3 or P6 ppm files."	*/
 
-int main(){
+int main(int args, char *argv[]){
+	if (args != 4) {
+		printf("Program requires: 'P# <inputname>.ppm <outputname>.ppm'");
+		return(1);
+	  }
+	  
 	printf("Hello World");
 	return(0);
 }
