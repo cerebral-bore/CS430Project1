1711269982e7d25468f1cca93c5b0288e40dff72
jjg297 <garciajj730@gmail.com>
2016-09-13 20:14:36 -0700

    Add files via upload
    Added a check for the ppm file to see if the ppm is p3 or p6
    Added a line scan for P-type

--- ppmrw.c
+++ ppmrw.c
@@ -7,16 +7,38 @@
 	"Code should be able to read or write P3 or P6 ppm files."	*/
 
 int main(int args, char *argv[]){
+	
+	char str[3];
+	
+	/* opening file for reading */
+    FILE *fh = fopen("output_3.ppm" , "r");
+	
+	// Code taken from Tutorialspoint.com C function library
+	if( fgets (str, 3, fh) != NULL ) 
+	{
+      /* writing content to stdout */
+      puts(str);
+	}
 
+	// Check to see if there are 3 input arguments on launch
 	if (args != 4) {
-		printf("Program requires: 'P# <inputname>.ppm <outputname>.ppm'");
+		fprintf(stderr, "Program requires: 'P# <inputname>.ppm <outputname>.ppm'");
 		return(1);
 	  }
+	  
 	// Check to see if the accepted P-type ppms are being asked for.
 	if((*argv[1] != '3') && (*argv[1] != '6')){
-		printf("This program only works with P3 and P6 type ppm files.");
+		fprintf(stderr, "This program only works with P3 and P6 type ppm files.");
 		return(2);
 	}
+	fprintf(stderr, str);
+	
+	// Check to see if the ppm file actually is P3 or P6
+	if((str[1] != '3') && (str[1] != '6')){
+		fprintf(stderr, "PPM file is not P3 or P6.");
+		return(3);
+	}
+	
 	printf("Hello World");
 	return(0);
 }
